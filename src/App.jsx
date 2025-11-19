01680b9f7f50c0150a4eca44eec7836d584686dd
Danish2404 <danishmohammed2404@gmail.com>
2025-11-19 12:01:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Item } from './Page/Item'
+import { Expense } from './Page/Expense'
+import { Addexpense } from './Page/Addexpense'
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Item/>} />
+        <Route path='/expense' element={<Expense/>} />
+        <Route path='/addexpense' element={<Addexpense/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
