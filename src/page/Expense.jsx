967561fd24c59d0657dbacc8d0ca3840b29968f8
Danish2404 <danishmohammed2404@gmail.com>
2025-11-21 13:18:13 +0530

    fix List path

--- src/page/Expense.jsx
+++ src/page/Expense.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Navbar } from '../Component/Navbar'
 import { Expensetotal } from '../Component/Expensetotal'
-import { List } from '../Component/list'
+import { List } from '../Component/List'
+
+
+
+
+
 
 export const Expense = () => {
   return (
